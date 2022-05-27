846d6d36d42e4dfe9b8057c06af8a2a04daa8a65
MuhammadBintang <m.bintangkhatulistiwa@gmail.com>
2022-05-28 00:30:47 +0700

    ui baru part 4

--- lib/konstant.dart
+++ lib/konstant.dart
@@ -3,5 +3,5 @@
 import 'package:flutter/material.dart';
 
 Color primaryColor = Color(0xFF050522);
-// String sUrl = "kutikin.000webhostapp.com";
-String sUrl = "192.168.43.145";
+String sUrl = "kutikin.000webhostapp.com";
+// String sUrl = "192.168.43.145";
