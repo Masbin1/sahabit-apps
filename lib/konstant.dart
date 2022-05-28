865066edb7bfe303d039e871a743e1db4ad04f7d
Mazbin1 <bintangbluzz@gmail.com>
2022-05-28 10:30:44 +0700

    last commit

--- lib/konstant.dart
+++ lib/konstant.dart
@@ -4,5 +4,5 @@ import 'package:flutter/material.dart';
 
 Color primaryColor = Color(0xFF050522);
 // String sUrl = "kutikin.000webhostapp.com";
-String sUrl = "192.168.43.145";
-// String sUrl = "http://localhost:80";
+// String sUrl = "192.168.43.145";
+String sUrl = "cism.sahabatmuseumkaa.com";
